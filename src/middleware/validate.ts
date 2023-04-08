92ff4c6114b1e1a3ac0c614bab34da7ac210e526
hoangcp <hoangcp8700@gmail.com>
2023-04-08 15:38:07 +0700

    :sparkles: Feat: custom multiple record for validate yup

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -3,11 +3,29 @@ import * as yup from "yup";
 
 import { HttpCode } from "@constants/enum";
 
+type ValidateFieldType = Record<string, any> | Record<string, any>[];
+
 const validateRequestBody =
-  <T extends object>(schema: yup.ObjectSchema<T>) =>
+  <T extends ValidateFieldType>(schema: yup.ObjectSchema<T> | yup.ArraySchema<any, any>) =>
   async (req: Request, res: Response, next: NextFunction) => {
+    const { body } = req;
     try {
-      await schema.validate(req.body, { abortEarly: false });
+      if (Array.isArray(body)) {
+        // Validate the multiple record
+        await Promise.all(
+          body.map((element, index) =>
+            schema.validate(element, { abortEarly: false }).catch((err) => {
+              err.inner.forEach((validationError: any) => {
+                validationError.path = `${validationError.path}[${index}]`;
+              });
+              throw err;
+            }),
+          ),
+        );
+      } else {
+        // Validate the single record
+        await schema.validate(body, { abortEarly: false });
+      }
       next();
     } catch (error: any) {
       const validationErrors = error.inner.reduce(
